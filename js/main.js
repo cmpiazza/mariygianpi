a0b071b219ed4f73d953eb730bed1a270df683e3
Catalina <catalinapiazza@gmail.com>
2019-12-27 19:10:49 -0300

    Fix(Timer)

--- js/main.js
+++ js/main.js
@@ -303,7 +303,7 @@
 
 	function makeTimer() {
 
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
+		var endTime = new Date("21 March 2020 21:00:00 GMT-03:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
